2e2c348ca46898e233cfd097d690c11836bed367
Andrii Lishchyna <andrej044@i.ua>
2023-06-28 17:06:43 -0400

    add style properties

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
 
   return(
     <div className='Logo ma4 mt0'>
-      <Tilt className='Tilt br2 shadow-2' glareEnable={true} gyroscope={true}>
+      <Tilt className='Tilt br2 shadow-2' glareEnable={true} gyroscope={true} scale={1.1}>
         <div>
           <img src={brain} alt='logo' />
         </div>
