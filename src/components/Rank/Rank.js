a2b1728f72ab9d980ee147c5d9b26d22b327bf9d
Andrii Lishchyna <andrej044@i.ua>
2023-06-28 17:56:11 -0400

    add basic structure

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,6 +3,12 @@ const Rank = () => {
 
   return(
     <div>
+      <div className="white f3">
+        {`Andrii, your current rank is...`}
+      </div>
+      <div className="white f1">
+        {`#5`}
+      </div>
     </div>
   )
 }
