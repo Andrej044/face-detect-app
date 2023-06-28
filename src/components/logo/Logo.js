f5283eda3bb8a0b96456fb9fc66887dc031f9a0b
Andrii Lishchyna <andrej044@i.ua>
2023-06-28 16:33:06 -0400

    import Logo.css to Logo component, build Tilt default structure

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,12 @@
 import Tilt from 'react-parallax-tilt';
+import "./Logo.css"
 
 const Logo = () => {
 
   return(
-    <div className='ma4 mt0'>
-      <Tilt>
-        <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
+    <div className='Logo ma4 mt0'>
+      <Tilt className='Tilt br2 shadow-2' glareEnable={true} gyroscope={true}>
+        <div>
           <h1>React Parallax Tilt 👀</h1>
         </div>
       </Tilt>
