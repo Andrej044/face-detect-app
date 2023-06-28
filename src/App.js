7e71c51ec00c010750982dbee6f11cf61fb87841
Andrii Lishchyna <andrej044@i.ua>
2023-06-28 16:08:05 -0400

    import logo component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
 
 
 function App() {
   return (
     <div className="App">
      <Navigation />
-     {/* <Logo />
-     <ImageLinkForm />
-     <FaceRecognition />} */}
+     <Logo />
+     {
+     /* <ImageLinkForm />
+     <FaceRecognition /> */}
     </div>
   );
 }
