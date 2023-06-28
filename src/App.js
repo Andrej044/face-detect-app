4362cff4f5264a4b3ab4fdfc08481311ad22052b
Andrii Lishchyna <andrej044@i.ua>
2023-06-28 17:16:47 -0400

    create ImageLinkForm component & import to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
 
 
 function App() {
   return (
     <div className="App">
-     <Navigation />
-     <Logo />
-     {
-     /* <ImageLinkForm />
-     <FaceRecognition /> */}
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+     {/*<FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -0,0 +1,16 @@
+import Tilt from 'react-parallax-tilt';
+
+const ImageLinkForm = () => {
+
+  return(
+    <div className='ma4 mt0'>
+      <Tilt className='Tilt br2 shadow-2' glareEnable={true} gyroscope={true} scale={1.1}>
+        <div>
+        <p>ImageLinkForm</p>
+        </div>
+      </Tilt>
+      </div>
+  )
+}
+
+export default ImageLinkForm
