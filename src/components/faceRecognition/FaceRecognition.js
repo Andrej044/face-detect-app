8eb78b3482b79096e537b805db46030c6c9ee21d
Andrii Lishchyna <andrej044@i.ua>
2023-06-30 22:32:05 -0400

    give position parametrs value from box props

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,14 +1,13 @@
-
+import "./FaceRecognition.css"
 
 const FaceRecognition = ({imageUrl, box}) => {
 
-  console.log(box)
   return(
     <div className="center ma">
       <div className="absolute mt2">
-        <img id="face-recognise-img" alt="" src={imageUrl} width='500px' height="auto0"/>
+        <img id="face-recognise-img" alt="" src={imageUrl} width='500px' height="auto"/>
+        <div className="face-box" style={{top:box.top, bottom:box.bottom, left:box.left, right:box.right}}></div>
       </div>
-      <div className="face-box"></div>
     </div>
   )
 }
