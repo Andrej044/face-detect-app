358f03e069105e6d9794d2e5ba0c4009c1b174b4
Andrii Lishchyna <andrej044@i.ua>
2023-06-30 21:08:47 -0400

    develop FaceRecognition component

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,9 +1,13 @@
 
 
-const FaceRecognition = () => {
+const FaceRecognition = ({imageUrl}) => {
 
   return(
-    <div>FaceRecognition</div>
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img alt="" src={imageUrl} width='500px' height="auto0"/>
+      </div>
+    </div>
   )
 }
 
