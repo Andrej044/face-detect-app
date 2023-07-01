5833ced704890393fe4691a8d0a71ebb138f4a23
Andrii Lishchyna <andrej044@i.ua>
2023-07-01 13:23:16 -0400

    solve bugs

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -8,11 +8,11 @@ const SignIn = () => {
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f4" for="email-address">Email</label>
+              <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f4" for="password">Password</label>
+              <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
               <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
           </fieldset>
@@ -20,7 +20,7 @@ const SignIn = () => {
             <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4 dib" type="submit" value="Sign in"/>
           </div>
           <div className="lh-copy mt3">
-            <a href="#0" class="f5 link dim black db">Register</a>
+            <a href="#0" className="f5 link dim black db">Register</a>
           </div>
         </form>
       </div>
