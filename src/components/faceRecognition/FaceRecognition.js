607ec961b8936149f4da430f6d07b575dc43c6b2
Andrii Lishchyna <andrej044@i.ua>
2023-06-30 22:15:29 -0400

    add div with class face-box

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,12 +1,14 @@
 
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
 
+  console.log(box)
   return(
     <div className="center ma">
       <div className="absolute mt2">
         <img id="face-recognise-img" alt="" src={imageUrl} width='500px' height="auto0"/>
       </div>
+      <div className="face-box"></div>
     </div>
   )
 }
