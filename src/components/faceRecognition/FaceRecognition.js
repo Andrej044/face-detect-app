5aa815d71770bd4002f1f83ca84629163175952d
Andrii Lishchyna <andrej044@i.ua>
2023-06-30 22:03:32 -0400

    add id to img

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({imageUrl}) => {
   return(
     <div className="center ma">
       <div className="absolute mt2">
-        <img alt="" src={imageUrl} width='500px' height="auto0"/>
+        <img id="face-recognise-img" alt="" src={imageUrl} width='500px' height="auto0"/>
       </div>
     </div>
   )
