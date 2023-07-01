5e7c15c209d15acaa676c06dbd30c8f6d88a414c
Andrii Lishchyna <andrej044@i.ua>
2023-07-01 13:41:38 -0400

    make logo position absolute

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import "./Logo.css";
 const Logo = () => {
 
   return(
-    <div className='Logo ma4 mt0'>
+    <div className='Logo ma4 absolute top--1 left--1'>
       <Tilt className='Tilt br2 shadow-2' glareEnable={true} gyroscope={true} scale={1.1}>
         <div>
           <img src={brain} alt='logo' />
