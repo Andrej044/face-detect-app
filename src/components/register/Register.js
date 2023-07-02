fc9ad3b1ae66e9b7a73b41adbc054bf3531fca37
Andrii Lishchyna <andrej044@i.ua>
2023-07-02 14:03:12 -0400

    add form structure for register form component

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,9 +1,33 @@
 
 
-const RegisterForm = () =>{
+const RegisterForm = ({onRouteChange}) =>{
 
   return(
-    <div>Register Form</div>
+    <div className="mw6 br3 pa3 pa4-ns mv3 ba b--black-10 shadow-5 center">
+      <div className="pa4 black-80">
+        <form className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f4" htmlFor="name">Name</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+            </div>
+          </fieldset>
+          <div className="">
+            <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f4 dib" type="submit" value="Sign in"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  
   )
 }
 
