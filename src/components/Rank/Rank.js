53f7c147f60e910a715f2d68530836b118fa33b1
Andrii Lishchyna <andrej044@i.ua>
2023-07-14 13:41:18 -0400

    get props name, entries and emplement it ni component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
 
   return(
     <div>
       <div className="white f3">
-        {`Andrii, your current rank is...`}
+        {`${name}, your current rank is...`}
       </div>
       <div className="white f1">
-        {`#5`}
+        {entries}
       </div>
     </div>
   )
