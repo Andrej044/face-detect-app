9ce072ba8cdffd1f4577db9418cb996c703b6105
Andrii Lishchyna <andrej044@i.ua>
2023-07-21 21:30:31 -0400

    implement multiface boxes into FaceRecognition project

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,12 +1,16 @@
 import "./FaceRecognition.css"
 
-const FaceRecognition = ({imageUrl, box}) => {
+const FaceRecognition = ({imageUrl, boxList}) => {
 
+  console.log(boxList) 
+  const faceBoxes = boxList.map( (box, index) => {
+    return  <div key={index} className="face-box" style={{top:box.top, bottom:box.bottom, left:box.left, right:box.right}}></div>
+  })
   return(
     <div className="center ma">
       <div className="absolute mt2">
         <img id="face-recognise-img" alt="" src={imageUrl} width='500px' height="auto"/>
-        <div className="face-box" style={{top:box.top, bottom:box.bottom, left:box.left, right:box.right}}></div>
+        {faceBoxes}
       </div>
     </div>
   )
